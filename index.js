625a58df7ca434bb3300e71edaecb011b1177b78
Rex Ogbemudia <rex.ogbemudia@andela.com>
2019-04-15 11:50:35 +0100

    refactor request

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const http = require('http');
-const bodyParser = require('body-parser');
+const router = require('router')();
 
 const Application = require('./lib/application');
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 const app = new Application(http.createServer());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req, res) => {
-  res.send(req.body);
+app.use((req, res, next) => {
+  console.log('middleware');
+  if (typeof next === 'function') {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  console.log('middleware2');
+  if (typeof next === 'function') {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  console.log('middleware3');
+  if (typeof next === 'function') {
+    next();
+  }
+});
+
+app.use(
+  router.get('/home/:id/:someotherid', (req, res, next) => {
+    console.log(req.params);
+    res.send({ msg: 'hello server 4 fun' });
+    next();
+  }),
+);
+
+app.use((req, res, next) => {
+  console.log('middleware3');
+  if (typeof next === 'function') {
+    next();
+  }
 });
 
 app.start(port);
